3bae0d946fa964ac93729b726670029d84fde381
Celina <luv.mins99@gmail.com>
2022-07-18 13:20:24 +0900

    feat: HttpSecurity 설정 추가

---
+++ src/main/java/com/practice/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.practice.jwttutorial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()        // HttpServletRequest를 사용하는 요청들에 대한 접근제한 설정
+                .antMatchers("/api/hello").permitAll()      // /api/hello 에 대한 요청은 인증없이 접근을 허용
+                .anyRequest().authenticated();      // 나머지 요청들은 모두 인증이 필요
+
+        return http.build();
+    }
+}
