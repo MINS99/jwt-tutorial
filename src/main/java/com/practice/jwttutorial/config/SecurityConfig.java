6b9ce6d7b73ef191ccea35983963c535c2b90898
Celina <luv.mins99@gmail.com>
2022-07-18 14:13:10 +0900

    feat: 로그인 상관 없이 허용을 해줘야 하는 리소스 설정 추가

--- src/main/java/com/practice/jwttutorial/config/SecurityConfig.java
+++ src/main/java/com/practice/jwttutorial/config/SecurityConfig.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().antMatchers("/h2-console/**", "/favicon.ico");
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
