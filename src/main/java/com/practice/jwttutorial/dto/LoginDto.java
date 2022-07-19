7fadc26584c9fadc3826eda4c07b716affbc45e9
Celina <luv.mins99@gmail.com>
2022-07-19 11:19:30 +0900

    feat: 요청 및 응답 객체 생성

---
+++ src/main/java/com/practice/jwttutorial/dto/LoginDto.java
@@ -0,0 +1,23 @@
+package com.practice.jwttutorial.dto;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+}
