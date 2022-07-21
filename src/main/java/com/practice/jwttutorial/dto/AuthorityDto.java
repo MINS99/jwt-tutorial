b10fc8dc4643a6cd34568bd65fba1d4cd5c4818a
Celina <luv.mins99@gmail.com>
2022-07-21 13:50:46 +0900

    feat: 유저의 권한 정보를 가지는 AuthorityDto 추가
    - 사용자 Dto에서도 가지도록 추가

---
+++ src/main/java/com/practice/jwttutorial/dto/AuthorityDto.java
@@ -0,0 +1,14 @@
+package com.practice.jwttutorial.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorityDto {
+    private String authorityName;
+}
--- src/main/java/com/practice/jwttutorial/dto/UserDto.java
+++ src/main/java/com/practice/jwttutorial/dto/UserDto.java
@@ -1,6 +1,9 @@
 package com.practice.jwttutorial.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.practice.jwttutorial.domain.User;
+import java.util.Set;
+import java.util.stream.Collectors;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -26,4 +29,18 @@ public class UserDto {
     @NotNull
     @Size(min = 3, max = 50)
     private String nickname;
+
+    private Set<AuthorityDto> authorityDtoSet;
+
+    public static UserDto from(User user) {
+        if(user == null) return null;
+
+        return UserDto.builder()
+                .username(user.getUsername())
+                .nickname(user.getNickname())
+                .authorityDtoSet(user.getAuthorities().stream()
+                        .map(authority -> AuthorityDto.builder().authorityName(authority.getAuthorityName()).build())
+                        .collect(Collectors.toSet()))
+                .build();
+    }
 }
