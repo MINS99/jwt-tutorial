2801683d4dd0fd516bc34cf6086d803361dd27c6
Celina <luv.mins99@gmail.com>
2022-07-21 13:52:38 +0900

    feat: 회원가입, 로그인 구현

---
+++ src/main/java/com/practice/jwttutorial/application/UserService.java
@@ -0,0 +1,53 @@
+package com.practice.jwttutorial.application;
+
+import com.practice.jwttutorial.domain.Authority;
+import com.practice.jwttutorial.domain.User;
+import com.practice.jwttutorial.dto.UserDto;
+import com.practice.jwttutorial.repository.UserRepository;
+import com.practice.jwttutorial.util.SecurityUtil;
+import java.util.Collections;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public UserDto signup(UserDto userDto) {
+        if (userRepository.findOneWithAuthoritiesByUsername(userDto.getUsername()).orElse(null) != null) {
+            throw new IllegalArgumentException("이미 가입되어 있는 유저입니다.");
+        }
+
+        Authority authority = Authority.builder()
+                .authorityName("ROLE_USER")
+                .build();
+
+        User user = User.builder()
+                .username(userDto.getUsername())
+                .password(passwordEncoder.encode(userDto.getPassword()))
+                .nickname(userDto.getNickname())
+                .authorities(Collections.singleton(authority))
+                .activated(true)
+                .build();
+
+        return UserDto.from(userRepository.save(user));
+    }
+
+    @Transactional(readOnly = true)
+    public UserDto getUserWithAuthorities(String username) {
+        return UserDto.from(userRepository.findOneWithAuthoritiesByUsername(username).orElse(null));
+    }
+
+    @Transactional(readOnly = true)
+    public UserDto getMyUserWithAuthorities() {
+        return UserDto.from(SecurityUtil.getCurrentUsername().flatMap(userRepository::findOneWithAuthoritiesByUsername).orElse(null));
+    }
+}
